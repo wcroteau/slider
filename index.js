4fedda1ee8dfc6a6604043edb0f3df3180989d78
Bill Croteau <bill@tunko.com>
2025-04-23 19:19:41 -0400

    Updates to track each slider independently.

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-const listEls = document.querySelectorAll(".list");
-const dotsEls = document.querySelectorAll(".dots");
 const prevButtonEls = document.querySelectorAll(".prev");
 const nextButtonEls = document.querySelectorAll(".next");
 
-let active = 0;
+const active = {
+  list0: 0,
+  list1: 0,
+  list2: 0,
+  list3: 0,
+  list4: 0,
+  list5: 0,
+};
+prevButtonEls.forEach((btn) =>
+  btn.addEventListener("click", () => slide(btn, -1))
+);
+nextButtonEls.forEach((btn) =>
+  btn.addEventListener("click", () => slide(btn, 1))
+);
 
-prevButtonEls.forEach((btn) => btn.addEventListener("click", () => slide(-1)));
-nextButtonEls.forEach((btn) => btn.addEventListener("click", () => slide(1)));
+function slide(btnEl, offset) {
+  const listId = btnEl.closest(".slider").querySelector(".list").dataset.listid;
 
-function slide(offset) {
-  console.log("Slide", offset);
-  active += offset;
-  if (active < 0) {
-    active = 4;
-  } else if (active > 4) {
-    active = 0;
+  active[`list${listId}`] += offset;
+  if (active[`list${listId}`] < 0) {
+    active[`list${listId}`] = 4;
+  } else if (active[`list${listId}`] > 4) {
+    active[`list${listId}`] = 0;
   }
 
-  const newLeft = `${active * -300}px`;
+  const newLeft = `${active[`list${listId}`] * -300}px`;
 
-  listEls.forEach((list) => {
-    list.querySelector(".items").style.left = newLeft;
-  });
+  btnEl.closest(".slider").querySelector(".items").style.left = newLeft;
 
-  dotsEls.forEach((dots) => {
-    dots.querySelectorAll("li").forEach((dot, i) => {
-      if (i === active) {
+  btnEl
+    .closest(".slider")
+    .querySelectorAll("li")
+    .forEach((dot, i) => {
+      if (i === active[`list${listId}`]) {
         dot.classList.add("active");
       } else {
         dot.classList.remove("active");
       }
     });
-  });
 }
